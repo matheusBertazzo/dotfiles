7ed78051a2a72b5497f7d56bc3bf52a591752f0c
Matheus Jatczak Bertazzo <matheus.bertazzo@cint.com>
2026-02-19 17:53:48 -0300

    Adds git-conflict plugin

---
+++ nvim/lua/plugins/git-conflict.lua
@@ -0,0 +1,14 @@
+return {
+	'akinsho/git-conflict.nvim',
+	version = "*",
+	opts = {
+		default_mappings = {
+			ours = '<leader>xo',
+			theirs = '<leader>xt',
+			none = '<leader>xn',
+			both = '<leader>xb',
+			next = ']x',
+			prev = '[x',
+		}
+	}
+}
